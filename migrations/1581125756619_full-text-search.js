1ba890b3dbbeb4562ecaf7cb5f0fc1dded347416
Junior Moraes <juniorfvox@gmail.com>
2020-02-08 00:05:51 -0200

    Add pg function to update tsv by artigo_id

---
+++ migrations/1581125756619_full-text-search.js
@@ -0,0 +1,48 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+  pgm.createExtension('unaccent', { ifNotExists: true });
+
+  pgm.addColumn('artigos',
+    { tsv: { type: 'tsvector' } },
+    { ifNotExists: true });
+
+  pgm.createFunction(
+    'f_artigos_tsv_update',
+    [
+      {
+        name: '_artigo_id',
+        type: 'int',
+      },
+    ],
+    {
+      returns: 'void',
+      language: 'plpgsql',
+    },
+    `declare
+      tags_str varchar;
+    begin
+
+      select string_agg(unaccent(tags.name), ' ') into tags_str
+      from artigos_tags__tags_artigos
+      join tags
+        on artigos_tags__tags_artigos.tag_id = tags.id
+      where artigos_tags__tags_artigos.artigo_id = _artigo_id;
+
+      update artigos
+        SET tsv = setweight(to_tsvector('portuguese', unaccent(title)), 'A')
+               || setweight(to_tsvector('portuguese', concat_ws(' ', author, organization)), 'B')
+               || setweight(to_tsvector('portuguese', coalesce(tags_str, '')), 'C')
+      WHERE id = _artigo_id;
+
+    end;`,
+  );
+};
+
+exports.down = pgm => {
+  pgm.dropColumn('artigos', 'tsv');
+
+  pgm.dropFunction('f_artigos_tsv_update', [], { ifExists: true });
+};
