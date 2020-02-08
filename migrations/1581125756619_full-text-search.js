852a3fd70bb389e5b0599d87ba7890dc3392ef82
Junior Moraes <juniorfvox@gmail.com>
2020-02-08 00:13:38 -0200

    Add gin index

--- migrations/1581125756619_full-text-search.js
+++ migrations/1581125756619_full-text-search.js
@@ -5,10 +5,18 @@ exports.shorthands = undefined;
 exports.up = pgm => {
   pgm.createExtension('unaccent', { ifNotExists: true });
 
+  // Add the tsvector column
   pgm.addColumn('artigos',
     { tsv: { type: 'tsvector' } },
     { ifNotExists: true });
 
+  // Create gin index on tsv column
+  pgm.createIndex('artigos', 'tsv', {
+    name: 'artigos_tsv_idx',
+    method: 'gin',
+  });
+
+  // Helper function to update tsvector column by artigo_id
   pgm.createFunction(
     'f_artigos_tsv_update',
     [
@@ -20,6 +28,7 @@ exports.up = pgm => {
     {
       returns: 'void',
       language: 'plpgsql',
+      replace: true,
     },
     `declare
       tags_str varchar;
@@ -32,17 +41,17 @@ exports.up = pgm => {
       where artigos_tags__tags_artigos.artigo_id = _artigo_id;
 
       update artigos
-        SET tsv = setweight(to_tsvector('portuguese', unaccent(title)), 'A')
+        set tsv = setweight(to_tsvector('portuguese', unaccent(title)), 'A')
                || setweight(to_tsvector('portuguese', concat_ws(' ', author, organization)), 'B')
                || setweight(to_tsvector('portuguese', coalesce(tags_str, '')), 'C')
-      WHERE id = _artigo_id;
+      where id = _artigo_id;
 
     end;`,
   );
 };
 
 exports.down = pgm => {
+  pgm.dropIndex('artigos', 'tsv', { name: 'artigos_tsv_idx' });
   pgm.dropColumn('artigos', 'tsv');
-
   pgm.dropFunction('f_artigos_tsv_update', [], { ifExists: true });
 };
